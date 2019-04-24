161a38418870139fd9fb0276901cade7d29276c1
rzx <770304867@qq.com>
2019-04-24 17:52:09 +0800

    细节修改

--- src/pages/about/views/Home.vue
+++ src/pages/about/views/Home.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png">
+
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "home",
-  data() {},
-  methods: {},
-  mounted() {}
-};
+  name: 'home',
+}
 </script>
--- src/pages/index/views/Home.vue
+++ src/pages/index/views/Home.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="@/assets/logo.png">
-
+    <h1>{{time}}</h1>
   </div>
 </template>
 
 <script>
-
+import { setInterval } from "timers";
 export default {
-  name: 'home',
-}
+  name: "home",
+  data() {
+    return {
+      time: ""
+    };
+  },
+  methods: {
+    getTimeStr() {
+      this.time = new Date().toLocaleString();
+    }
+  },
+  mounted() {
+    setInterval(()=> {
+      this.getTimeStr();
+    }, 1000);
+  },
+  computed() {},
+  watch() {}
+};
 </script>
