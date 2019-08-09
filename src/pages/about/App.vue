9e6cada7bdcab8149b62af8b25abbc4593a215ca
rzx <770304867@qq.com>
2019-08-09 14:04:56 +0800

    多页面跳转

--- src/pages/about/App.vue
+++ src/pages/about/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <a href="index.html">Index</a>|
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
--- vue.config.js
+++ vue.config.js
@@ -6,15 +6,16 @@ function muliti() {
     const pages = {}
     glob.sync('./src/pages/**/main.js').forEach(path => {
         const chunk = path.split('./src/pages/')[1].split('/main.js')[0];//设置名称
-        console.log(path.split('/main.js')[0]+'/'+chunk+'.html');
         pages[chunk] = {
             entry: path,
             // template: 'public/'+chunk+'.html',
             template: path.split('/main.js')[0]+'/'+chunk+'.html',
             title: chunk,
-            chunks: ['chunk-vendors', 'chunk-common', 'index']
+            filename:chunk+'.html',
         }
     })
+    console.log(pages);
+    
     return pages
 }
 module.exports = {
